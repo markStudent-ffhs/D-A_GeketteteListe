50d0766a3d0ef1500cdf413dcaddbb0b894b46cc
Mark Achermann <achermann@devchannel.ch>
2017-08-28 22:15:25 +0200

    first code commit

---
+++ src/ch/ffhs/dua/pva/list/ListBasic.java
@@ -0,0 +1,88 @@
+package ch.ffhs.dua.pva.list;
+/**
+ * Basis Implementierung des List Interfaces;
+ * Alle Methoden werfen eine Exception.
+ * Kann verwendet werden als Basisklasse einer 
+ * teilweisen Implementierung des ListenInterfaces.
+ */
+
+import java.util.Collection;
+import java.util.List;
+import java.util.ListIterator;
+
+public abstract class ListBasic<E> implements List<E>
+{
+
+    @Override
+    public boolean containsAll(Collection<?> c)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int lastIndexOf(Object o)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ListIterator<E> listIterator()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex)
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public int indexOf(Object o)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+}
